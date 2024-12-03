7356d0a6cad5537dabf9bd19d378c59fc0ac95e1
everlast1ngw1nter <at07012004@gmail.com>
2024-12-03 15:23:06 +0500

    1 DONE

---
+++ BadNews/Controllers/CommentsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using BadNews.Models.Comments;
+using BadNews.Repositories.Comments;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BadNews.Controllers
+{
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly CommentsRepository commentsRepository;
+
+        public CommentsController(CommentsRepository commentsRepository)
+        {
+            this.commentsRepository = commentsRepository;
+        }
+
+        // GET
+        [HttpGet("api/news/{id}/comments")]
+        public ActionResult<CommentsDto> GetCommentsForNews(Guid newsId)
+        {
+            var comments = commentsRepository
+                .GetComments(newsId)
+                .Select(c => new CommentDto { User = c.User, Value = c.Value })
+                .ToList();
+            return new CommentsDto { NewsId = newsId, Comments = comments};
+        }
+    }
+}
